e65d692c738e62f5c1df771b9a5291c27a625890
Gakuto1112 <gakuto131112@gmail.com>
2021-12-13 14:26:16 +0900

    Botがログインするまで実装

--- MinecraftDiscordChatSync.ts
+++ MinecraftDiscordChatSync.ts
@@ -1,14 +1,22 @@
 const fs = require("fs");
+const {Client, Intents } = require("discord.js");
+const client = new Client({ intents: Object.keys(Intents.FLAGS) });
 let settings: { [key: string]: any } = { };
 const colors: { [key: string]: string } = { black:"\u001b[30m", red: "\u001b[31m", green: "\u001b[32m", yellow: "\u001b[33m", blue: "\u001b[34m", magenta: "\u001b[35m", cyan: "\u001b[36m", white: "\u001b[37m", reset: "\u001b[0m" }; //標準出力に色を付ける制御文字
 
 //設定ファイルの存在確認
 if(fs.existsSync("Settings.json")) {
     settings = JSON.parse(fs.readFileSync("Settings.json", "utf-8"));
-    console.log(settings);
 } else {
     const settingsPattern: { [key: string]: any } = { "token": "" };
-    fs.writeFileSync("Settings.json", JSON.stringify(settingsPattern))
+    fs.writeFileSync("Settings.json", JSON.stringify(settingsPattern, null, 4))
     console.log(colors.yellow + "設定ファイルの「Settings.json」が存在しません。\n" + colors.reset + "「Settings.json」を生成しました。ファイルを開いて必要な情報を入力して下さい。");
     process.exit(0);
 }
+
+//Botへのログイン
+client.login(settings.token).catch((error: any) => {
+    const errorName: string = error.name;
+    if(errorName == "Error [TOKEN_INVALID]") console.log(colors.red + "エラーが発生しました。トークンが無効です。正しいトークンが設定されているか確認して下さい。" + colors.reset);
+    process.exit(1);
+});

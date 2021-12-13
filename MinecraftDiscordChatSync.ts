2cc22059a04e26835ee3f9e1ca7f99fa39b92deb
Gakuto1112 <gakuto131112@gmail.com>
2021-12-13 14:43:43 +0900

    必要と思われるインテントのみに限定

--- MinecraftDiscordChatSync.ts
+++ MinecraftDiscordChatSync.ts
@@ -1,6 +1,6 @@
 const fs = require("fs");
-const {Client, Intents } = require("discord.js");
-const client = new Client({ intents: Object.keys(Intents.FLAGS) });
+const { Client } = require("discord.js");
+const client = new Client({ intents: ["GUILD_MESSAGES"] });
 let settings: { [key: string]: any } = { };
 const colors: { [key: string]: string } = { black:"\u001b[30m", red: "\u001b[31m", green: "\u001b[32m", yellow: "\u001b[33m", blue: "\u001b[34m", magenta: "\u001b[35m", cyan: "\u001b[36m", white: "\u001b[37m", reset: "\u001b[0m" }; //標準出力に色を付ける制御文字
 

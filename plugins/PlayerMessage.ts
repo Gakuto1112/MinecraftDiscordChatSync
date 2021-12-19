0f8f1ca052c59b2dceb4d47969d1062648c248ce
Gakuto1112 <gakuto131112@gmail.com>
2021-12-19 22:48:37 +0900

    プレイヤー発言時のメッセージ送信のクラス追加

---
+++ plugins/PlayerMessage.ts
@@ -0,0 +1,13 @@
+import { PluginBase } from "./PluginBase";
+import { sendMessageToDiscord } from "../MinecraftDiscordChatSync";
+
+export class Plugin extends PluginBase {
+    public onMinecraftMessage(time: Date, thread: string, messageType: string, message: string): void {
+        if(/^<\w{3,16}> /.test(message)) {
+            const messageSplit: string[] = message.split(" ");
+            if(messageSplit.length >= 2) {
+                sendMessageToDiscord(":speech_balloon: <" + messageSplit[0].slice(1, -1) + "> " + messageSplit.slice(1).join(" "));
+            }
+        }
+    }
+}

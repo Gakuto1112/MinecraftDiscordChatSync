03bdfb7f2c67dd0fb6c42c11a333849a291710d6
Gakuto1112 <gakuto131112@gmail.com>
2022-06-11 14:23:00 +0900

    死亡メッセージの修正

--- plugins/Advancements.ts
+++ plugins/Advancements.ts
@@ -25,7 +25,7 @@ export class Plugin extends PluginBase {
             else console.error(colors.red + "「./plugins/data/advancements.tsv」を読み取れません。エラーコード：" + error.code + colors.reset);
             throw Error();
         }
-        data.split(/\n/).forEach((line: string, i: number) => {
+        data.split(/\n|\r|\r\n/).forEach((line: string, i: number) => {
             if(i >= 1) {
                 const record: AdvancementObject = { id: "",  name: "", description: "" };
                 const lineSplit = line.split("\t");
